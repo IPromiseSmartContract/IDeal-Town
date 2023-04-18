bfc2d5998b313d23a6bc023d6b9e5d95f154740a
WaterSo0910 <jacky950141@gmail.com>
2023-04-18 08:12:51 +0800

    feat: home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,50 +1,92 @@
 <script setup lang="ts">
+import Card from 'primevue/card'
 import Button from 'primevue/button'
-import Menubar from 'primevue/menubar'
-import { ref } from 'vue'
-import LogoSvg from '@/components/LogoSvg.vue'
-const items = ref([
-  {
-    label: 'Idea',
-    icon: 'pi pi-fw pi-pencil'
-    // items: []
-  },
-  {
-    label: 'Developer',
-    icon: 'pi pi-fw pi-user'
-  },
-  {
-    label: 'Doc',
-    icon: 'pi pi-fw pi-file'
+
+import { computed, reactive, ref, onMounted } from 'vue'
+import Tag from 'primevue/tag'
+import { useRouter } from 'vue-router'
+const router = useRouter()
+
+interface IProject {
+  id: number
+  name: string
+  status: string
+  address: string
+}
+const projects = reactive<IProject[]>([])
+const generateProjects = async () => {
+  const statusList = ['completed', 'active', 'inactive']
+  for (let i = 1; i <= 20; i++) {
+    const project: IProject = {
+      id: i,
+      name: `Project ${i}`,
+      status: statusList[Math.floor(Math.random() * statusList.length)],
+      address: `0x${Math.floor(Math.random() * 1000000).toString(16)}`
+    }
+    projects.push(project)
   }
-])
+}
+onMounted(async () => {
+  await generateProjects()
+})
+const activeProjects = computed(() => {
+  // Filter projects by status
+  return projects.filter((project) => project.status !== 'ina1ctive')
+})
+
+const getStatusStyle = (status: string) => {
+  switch (status) {
+    case 'completed':
+      return {
+        backgroundImage: 'linear-gradient(120deg, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%)'
+      }
+    case 'active':
+      return {
+        backgroundImage: 'linear-gradient(120deg, rgba(252,70,107,1) 0%, rgba(63,94,251,1) 100%)'
+      }
+    case 'inactive':
+      return {
+        backgroundImage: 'linear-gradient(120deg, rgba(0,0,0,1) 0%, rgba(10,50,100,1) 100%)'
+      }
+    default:
+      return {}
+  }
+}
+const goToProjectView = (project: IProject) => {
+  router.push(`/project/${project.address}`)
+}
+const goToProjectListView = () => {
+  router.push(`/project`)
+}
 </script>
 
 <template>
-  <header>
-    <Menubar :model="items" style="background-color: white; padding: 20px 10px 15px 10px">
-      <template #start>
-        <LogoSvg :width="40" :height="40" />
-      </template>
-      <template #end>
-        <Button>Connect Wallet</Button>
-      </template>
-    </Menubar>
-  </header>
-  <main></main>
+  <div class="section">
+    <div
+      class="flex justify-content-center card-container pt-5 text-5xl font-bold scalein animation-duration-1000"
+    >
+      Deal it, and own it
+    </div>
+    <div
+      class="flex justify-content-center card-container py-5 text-xl scalein animation-duration-1000"
+    >
+      Ideal town make idea come true
+    </div>
+    <div class="flex justify-content-center card-container gap-4 mt-6">
+      <Button
+        icon="pi pi-check"
+        label="Join"
+        style="background-color: #eebc63; border: 0; color: #000"
+        @click="(e:Event)=>goToProjectListView()"
+      />
+      <Button icon="pi pi-times" label="More" style="background-color: rgba(70, 58, 58, 0.8)" />
+    </div>
+  </div>
 </template>
 <style scoped>
-:deep(.p-menubar .p-menubar-end) {
-  margin-left: 2px !important;
-}
-:deep(.p-menubar) {
-  border: 0px solid #dee2e6;
-  border-radius: 0px;
-  display: flex;
-  align-items: center;
-  flex-wrap: nowrap;
-  flex-direction: row;
-  align-content: flex-start;
-  justify-content: space-between;
+.section {
+  font-family: 'Allerta Stencil';
+  margin-top: 50px;
+  margin-bottom: 500px;
 }
 </style>
