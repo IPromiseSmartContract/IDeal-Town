c460fd1d885533c012ed77e98f4accc0ab5c91ce
WaterSo0910 <jacky950141@gmail.com>
2023-04-19 04:58:25 +0800

    feat: add editor

---
+++ src/components/MdEditor.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import MdEditor, { type ToolbarNames } from 'md-editor-v3'
+import 'md-editor-v3/lib/style.css'
+const props = defineProps({
+  modelValue: {
+    type: String,
+    required: true
+  }
+})
+
+const text = ref(props.modelValue)
+const toolbars: ToolbarNames[] = [
+  'bold',
+  'underline',
+  'italic',
+  '-',
+  'strikeThrough',
+  'title',
+  'sub',
+  'sup',
+  'quote',
+  'unorderedList',
+  'orderedList',
+  'task', // ^2.4.0
+  '-',
+  'codeRow',
+  'code',
+  'link',
+  'image',
+  'table',
+  'mermaid',
+  'katex',
+  '=',
+  'revoke',
+  'next',
+  'save',
+  //   'pageFullscreen',
+  //   'fullscreen',
+  'preview',
+  //   'htmlPreview'
+  'catalog'
+  //   'github'
+]
+</script>
+
+<template>
+  <MdEditor
+    v-model="text"
+    language="en-US"
+    previewTheme="cyanosis"
+    :toolbars="toolbars"
+    codeTheme="atom"
+    :autoDetectCode="true"
+    style="background-color: transparent; border: 2px solid; min-height: 60vh"
+  />
+</template>
+<style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,13 @@ import 'primevue/resources/themes/lara-light-indigo/theme.css' //theme
 import 'primevue/resources/primevue.min.css' // core
 import 'primeicons/primeicons.css' //icons
 import 'primeflex/primeflex.css'
+import ToastService from 'primevue/toastservice';
 
 import './assets/main.css'
 
 const app = createApp(App)
 
+app.use(ToastService);
 app.use(createPinia())
 app.use(router)
 app.use(PrimeVue)
