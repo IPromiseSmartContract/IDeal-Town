dc40b2ef1ac608e2b70d98e6d688a2a21abb72e7
WaterSo0910 <jacky950141@gmail.com>
2023-04-18 08:11:51 +0800

    feat: make it prettier

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import router from './router'
 import 'primevue/resources/themes/lara-light-indigo/theme.css' //theme
 import 'primevue/resources/primevue.min.css' // core
 import 'primeicons/primeicons.css' //icons
+import 'primeflex/primeflex.css'
 
 import './assets/main.css'
 
