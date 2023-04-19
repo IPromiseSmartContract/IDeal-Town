b282cb8aad8767e3a704da20b5ee453484a54402
alan890104 <alan890104@gmail.com>
2023-04-19 16:07:53 +0800

    feat: initialize template for project contract

--- contracts/Project.sol
+++ contracts/Project.sol
@@ -7,35 +7,128 @@ import "./IPJToken.sol";
 
 interface IToken {
     function mint(address account, uint256 amount) external;
+
     function burn(address account, uint256 amount) external;
 }
 
 contract Project {
+    enum Stages {
+        Open, // proposer open the project and developers can submit their solution
+        Vote, // proposer vote for their favorite solution(s)
+        Review, // reviewers review the solution and send reputation by unirep
+        Reward, // proposer claim reward after the project is expired
+        Closed // project is closed
+    }
+
+    struct Proposal {
+        uint256 index; // index of this proposal
+        string url; // url of the proposal
+    }
+
     string public name;
-    address public owner;
+    address public proposer;
     uint256 public expiration;
     uint256 public threshold;
     address public tokenAddress;
-    
-    constructor(string memory _name, address _owner, uint256 _expiration, uint256 _threshold, uint256 _count, uint256 amount) {
-        IPJToken Token = new IPJToken(string.concat("IPJName#", Strings.toString(_count)), string.concat("IPJSymbol#", Strings.toString(_count)), amount);
+    uint256 public totalProposals;
+    Stages public currentStage;
+
+    mapping(address => Proposal) public proposals;
+    mapping(address => uint256) public votes;
+
+    modifier onlyProposer() {
+        require(
+            msg.sender == proposer,
+            "Only proposer can call this function."
+        );
+        _;
+    }
+    modifier requireStage(Stages _stage) {
+        require(currentStage == _stage, "Project is not in the right stage.");
+        _;
+    }
+
+    event URLSubmitted(address indexed sender, string url);
+    event Voted(address indexed sender, uint256 proposalIndex);
+    event Reviewed(address indexed sender, uint256 proposalIndex);
+    event RewardClaimed(address indexed sender, uint256 proposalIndex);
+
+    constructor(
+        string memory _name,
+        address _proposer,
+        uint256 _expiration,
+        uint256 _threshold,
+        uint256 _count,
+        uint256 _amount,
+        string memory proposalURL
+    ) {
+        IPJToken Token = new IPJToken(
+            string.concat("IPJName#", Strings.toString(_count)),
+            string.concat("IPJSymbol#", Strings.toString(_count)),
+            _amount
+        );
         name = _name;
-        owner = _owner;
+        proposer = _proposer;
         expiration = _expiration;
         threshold = _threshold;
         tokenAddress = address(Token);
+        currentStage = Stages.Open;
+
+        submitURL(_proposer, proposalURL);
     }
 
-    function mintToken(uint256 amount) public{
+    function mintToken(uint256 amount) public {
         IToken(tokenAddress).mint(msg.sender, amount);
     }
 
-    function burnToken(uint256 amount) public{
+    function burnToken(uint256 amount) public {
         IToken(tokenAddress).burn(msg.sender, amount);
     }
 
-    function getTokenAddress() public view returns(address) {
+    function getTokenAddress() public view returns (address) {
         return tokenAddress;
     }
-}
 
+    // @ Test
+    // setCurrentStage change the current stage of the project manually without checking
+    // Notice that this function is for testing purpose only
+    // 0: Open
+    // 1: Vote
+    // 2: Review
+    // 3: Reward
+    // 4: Closed
+    function setCurrentStage(uint _stage) public {
+        currentStage = Stages(_stage);
+    }
+
+    // TODO: register as a proposer or developer
+    // @ Alan
+    function register() external {}
+
+    // TODO: receive file url from proposer's proposal and developer's solution
+    // You should increase the total proposal count, push the url to the proposals mapping and emit URLSubmitted event
+    // @ Maxie
+    function submitURL(
+        address sender,
+        string memory url
+    ) public requireStage(Stages.Open) {}
+
+    // TODO: proposer vote for their favorite solution(s)
+    // You should emit Voted event if the proposal is accepted
+    // @ Maxie
+    function voteForSolution()
+        external
+        onlyProposer
+        requireStage(Stages.Vote)
+    {}
+
+    // TODO: reviewers review the solution and send reputation by unirep
+    // You should emit Reviewed event if the solution is accepted
+    // @ Alan
+    function review() external requireStage(Stages.Review) {}
+
+    // TODO: claim reward after the project is expired
+    // You should emit RewardClaimed event
+    // @ Maxie
+    function claimReward() external requireStage(Stages.Reward) {}
+}
--- contracts/ProjectFactory.sol
+++ contracts/ProjectFactory.sol
@@ -6,22 +6,36 @@ import "./Project.sol";
 contract ProjectFactory {
     address[] public ProjectAddress;
     uint256 _count;
-    
+
     // count for project number.
     constructor() {
         _count = 0;
     }
 
-    function createProject(string memory name, uint256 amount, uint256 expiration, uint256 threshold) public {
+    function createProject(
+        string memory name,
+        uint256 amount,
+        uint256 expiration,
+        uint256 threshold,
+        string memory proposalURL // url of the proposal
+    ) public {
         // deploy project contract and push project address into ProjectAddress list.
-        Project project = new Project(name, address(this), expiration, threshold, _count, amount);
+        Project project = new Project(
+            name,
+            msg.sender,
+            expiration,
+            threshold,
+            _count,
+            amount,
+            proposalURL
+        );
         ProjectAddress.push(address(project));
 
         // add project number
         _count++;
     }
 
-    function count() view public returns(uint256){
+    function count() public view returns (uint256) {
         return _count;
     }
 }
