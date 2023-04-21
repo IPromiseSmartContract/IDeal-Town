c48ee4de9a81f5bb526b202f9bf6c4c34bf6b802
parker <parker178912.en08@nycu.edu.tw>
2023-04-21 16:35:22 +0800

    add comment

--- contracts/Dao.sol
+++ contracts/Dao.sol
@@ -25,6 +25,7 @@ contract Dao {
     }
     Candidates[] public candidates;
     
+    // tokenAddress: IDT Token address
     constructor(address _tokenAddress) {
         owner = msg.sender;
         tokenAddress = _tokenAddress;
@@ -32,14 +33,18 @@ contract Dao {
 
     function startVoting(uint256 _durationMinutes, address[] memory _candidateAddress) public {
         require(!voteStage, "There are still a voting exist.");
+        // clean origin reviewer list
         delete reviewerList;
-        // Vote count start from 0
+        // candidates amount start from 0
         for (uint i = 0; i < _candidateAddress.length; i++) {
             Candidates memory _newCandidate = Candidates(_candidateAddress[i], 0);
             candidates.push(_newCandidate);
         }
+        // num of candidates in this vote
         candidateNum = _candidateAddress.length;
+        // end voting time
         endTime = block.timestamp + (_durationMinutes * 1 minutes);
+        // start vote -> vote stage = true
         voteStage = true;
     }
 
@@ -48,6 +53,7 @@ contract Dao {
         require(block.timestamp < endTime, "Voting has ended.");
         bool firstVoted = true;
         uint256 voterPosition;
+        // if the voter is not first voted then you can find it in voters and then find its position
         for (uint i = 0; i < voters.length; i++) {
             if (voters[i].Address == msg.sender) {
                 firstVoted = false;
@@ -55,33 +61,46 @@ contract Dao {
                 break;
             }
         }
+
+        // if the voter is first voted then check his balance of IDT token and you push him into voters 
         if (firstVoted) {
             Voters memory _newVoter = Voters(msg.sender, IToken(tokenAddress).balanceOf(msg.sender));
             voters.push(_newVoter);
             voterPosition = voters.length-1;
         }
+
+        // if voter balance less than voteAmount, revert the vote
         require(voters[voterPosition].tickets>_voteAmount, "You don't have enough tickets !");
-        uint256 count = 0;
+        bool candidateExist = false;
+        
+        // find the specific candidate and give them the tickets
         for (uint256 i = 0; i < candidates.length; i++) {
             if (candidates[i].Address == _candidateAddress) {
                 candidates[i].getAmount += _voteAmount;
                 voters[voterPosition].tickets -= _voteAmount;
-                count = 1;
+                candidateExist = true;
                 break;
             }
         }
-        require(count!=0, "Invalid candidate address.");
-    }
 
+        // didn't find the candidate
+        require(candidateExist, "Invalid candidate address.");
+    }
+    
     function endVoting(uint electAmount) public {
         require(block.timestamp >= endTime, "Voting has not ended yet.");
         require(voteStage, "The voting has already settled.");
         voteStage = false;
+
+        // electAmount: the minimum tickets that need to have
+        // if tickets > electAmount then push the candidate into review list
         for (uint i = 0; i < candidateNum; i++) {
             if (candidates[i].getAmount > electAmount) {
                 reviewerList.push(candidates[i].Address);          
             }
         }
+        
+        // clean candidates and voters list
         delete candidates;
         delete voters;
     }
