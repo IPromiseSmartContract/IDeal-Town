b43a4778d9c133bbed365fe2cc8c4afd1d516d20
WaterSo0910 <jacky950141@gmail.com>
2023-04-21 10:11:49 +0800

    feat: vote stage

--- frontend/src/components/Steps/ProjectVote.vue
+++ frontend/src/components/Steps/ProjectVote.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { useRoute } from 'vue-router'
 import { computed, onMounted, reactive } from 'vue'
 import Tag from 'primevue/tag'
 import InputNumber from 'primevue/inputnumber'
@@ -8,34 +7,9 @@ import { getStatusStyle } from '@/utils/style'
 import MdView from '@/components/MdView.vue'
 import Card from 'primevue/card'
 import { useToast } from 'primevue/usetoast'
-import { useField, useForm } from 'vee-validate'
 
-const { handleSubmit, resetForm } = useForm()
-const { value, errorMessage, errors } = useField('value', validateField)
 const toast = useToast()
 
-function validateField(value) {
-    if (!value) {
-        return 'Year is required.'
-    }
-
-    return true
-}
-
-const onSubmit = handleSubmit((values, actions) => {
-    if (values.value && values.value >= 1960 && values.value <= 2050) {
-        toast.add({ severity: 'info', summary: 'Form Submitted', detail: values.value, life: 3000 })
-        resetForm()
-    } else {
-        actions.setErrors({ value: 'Enter a valid year.' })
-    }
-})
-const route = useRoute()
-const address = computed(() => route.params.address)
-const wallet = reactive({
-    idt: 120,
-    ipj: 200
-})
 const project = reactive({
     name: 'Project name',
     mdContent: `
@@ -64,12 +38,15 @@ The project aims to create a comprehensive software platform that can be used to
     status: 'active',
     currentIpj: 1000
 })
-
+const wallet = reactive({
+    idt: 120,
+    ipj: 200
+})
 interface ISolution {
     id: number
     name: string
     link: string
-    voteIdc: number
+    voteIpj: number
 }
 const solutions = reactive<ISolution[]>([])
 const generateSolutions = async () => {
@@ -78,14 +55,42 @@ const generateSolutions = async () => {
             id: i,
             name: `Solution ${i}`,
             link: `https://ipfs.io/ipfs/${Math.floor(Math.random() * 1000000).toString(16)}`,
-            voteIdc: 0
+            voteIpj: 0
         }
         solutions.push(project)
+    }
+}
+const sendVoteTx = () => {}
+const handleSubmit = (afterSubmitFunc: Function) => {
+    if (!checkEnoughIpj(totalVotedIpj.value)) {
+        toast.add({
+            severity: 'error',
+            summary: 'Submit',
+            detail: 'Not enough Ipj to vote',
+            life: 5000
+        })
+    } else {
+        toast.add({
+            severity: 'success',
+            summary: 'Submit',
+            detail: 'vote',
+            life: 5000
+        })
+        afterSubmitFunc()
     }
 }
 onMounted(async () => {
     await generateSolutions()
 })
+const totalVotedIpj = computed(() => {
+    const votedIpj = solutions.reduce((sum, solution) => sum + solution.voteIpj, 0)
+    return votedIpj
+})
+const checkEnoughIpj = (votedIpj: number) => {
+    console.log(votedIpj, wallet.ipj)
+
+    return votedIpj < wallet.ipj
+}
 </script>
 <template>
     <div class="card mx-4">
@@ -101,12 +106,21 @@ onMounted(async () => {
         </div>
         <!-- Solution -->
         <div class="p-title grid mt-5 p-2 mx-1 align-items-center justify-content-between">
-            <h4>Solution</h4>
-            <Button size="small" class="p-btn shadow-3">Solve</Button>
+            <h4 class="ml-2">Vote</h4>
+            <h4>
+                <span class="text-xs">Voted</span> {{ totalVotedIpj }}
+                <span class="text-xs">IPJ</span>
+            </h4>
+            <h4>
+                <span class="text-xs">Total</span> {{ wallet.ipj }} <span class="text-xs">IPJ</span>
+            </h4>
+            <Button size="small" class="p-btn shadow-3" @click="handleSubmit(sendVoteTx)"
+                >Confirm</Button
+            >
         </div>
         <div class="p-body mt-0 p-2 mx-1 flex flex-column gap-3">
             <div v-for="solution in solutions" :key="solution.id" class="grid">
-                <div class="col-8">
+                <div class="col-12 md:col-8">
                     <Card
                         class="border-round justify-content-center w-full shadow-2 hover:shadow-8"
                         style="background-color: inherit"
@@ -133,19 +147,12 @@ onMounted(async () => {
                         </template>
                     </Card>
                 </div>
-                <div class="col-4">
-                    <form @submit="onSubmit" class="flex flex-column gap-2">
-                        <label for="year">Enter a year between 1960-2050.</label>
-                        <InputNumber
-                            v-model="solution.voteIdc"
-                            :class="{ 'p-invalid': errorMessage }"
-                            :useGrouping="false"
-                        />
-                        <small class="p-error" id="number-error">{{
-                            errorMessage || (errors.length > 0 ? errors : '&nbsp;')
-                        }}</small>
-                        <Button type="submit" label="Submit" />
-                    </form>
+                <div class="col-12 md:col-4 flex justify-content-center align-items-center">
+                    <InputNumber
+                        v-model="solution.voteIpj"
+                        :class="{ 'p-invalid': checkEnoughIpj(solution.voteIpj) }"
+                        :useGrouping="false"
+                    />
                 </div>
             </div>
         </div>
@@ -169,11 +176,14 @@ onMounted(async () => {
     color: rgb(238, 188, 99) !important;
     font-size: small;
 }
+::v-deep(.p-component) {
+    font-family: 'Rubik', sans-serif;
+}
 .p-title {
     border: 2px solid rgb(70, 58, 58);
     color: rgb(59, 48, 48);
-    font-size: large;
-    font-family: 'Allerta Stencil';
+    font-size: x-large;
+    font-family: 'Rubik', sans-serif;
 }
 .p-body {
     border: 1px solid rgb(70, 58, 58);
